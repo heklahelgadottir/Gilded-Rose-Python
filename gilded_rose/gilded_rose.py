4292a731796d3605f977c6c19724ec6e39a7fc56
Ross Pavitt <ross.pavitt@aexp.com>
2021-05-06 11:52:47 +0100

    Add legendary flag

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -5,10 +5,11 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+            isLegendary = item.name == "Sulfuras, Hand of Ragnaros"
             itemIncreasesInValue = item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert"
             if not itemIncreasesInValue:
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                    if not isLegendary:
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
@@ -20,13 +21,13 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if not isLegendary:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if not isLegendary:
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
